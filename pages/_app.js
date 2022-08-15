d51211aca623d104781576e6440d18c3f8594be7
MahmoudKamal01 <dddd>
2022-08-15 16:29:27 +0200

    c

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import "../styles/navbar.css";
 import "../styles/footer.css";
 import "../styles/404.css";
 import "../styles/home.css";
-import "../font-awesome/css/font-awesome.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (

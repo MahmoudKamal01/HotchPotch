059d38f4e688782eac907b169c68ff5875ba86af
MahmoudKamal01 <53492856+MahmoudKamal01@users.noreply.github.com>
2022-08-15 04:13:32 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
           <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
           <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+          <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
           <script
             src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}
           ></script>
